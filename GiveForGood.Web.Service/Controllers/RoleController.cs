46ef25cc14535c379752d78ff947019c7178cdea
sankar reddy avula <sankar.avula@betalen.in>
2024-03-21 21:54:29 +0530

    Role functionalities

---
+++ GiveForGood.Web.Service/Controllers/RoleController.cs
@@ -0,0 +1,58 @@
+﻿using GiveForGood.Web.Service.Interfaces;
+using GiveForGood.Web.Service.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GiveForGood.Web.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly IRoleService roleService;
+        public RoleController(IRoleService _roleService)
+        {
+            this.roleService = _roleService;
+        }
+
+        [HttpGet]
+        [Route("GetAllRoles")]
+        public async Task<IActionResult> GetAllRoles()
+        {
+            var roles = await roleService.GetAllRoles();
+            return Ok(roles);
+        }
+
+        [HttpPost]
+        [Route("AddRole")]
+        public async Task<IActionResult> AddRole(Role role)
+        {
+            var responce = await roleService.AddRole(role);
+            return Ok(responce);
+        }
+
+        [HttpPut]
+        [Route("UpdateRole")]
+        public async Task<IActionResult> UpdateRole(long roleId,Role role)
+        {
+            var responce = await roleService.UpdateRole(roleId, role);
+            return Ok(responce);
+        }
+
+        [HttpDelete]
+        [Route("DeleteRole")]
+        public async Task<IActionResult> DeleteRole(long roleId)
+        {
+            var responce = await roleService.DeleteRole(roleId);
+            return Ok(responce);
+        }
+
+        [HttpGet]
+        [Route("GetRoleById")]
+        public async Task<IActionResult> GetRoleById(long roleId)
+        {
+            var role = await roleService.GetRoleById(roleId);
+            return Ok(role);
+        }
+    }
+}
---
+++ GiveForGood.Web.Service/Interfaces/IRoleService.cs
@@ -0,0 +1,13 @@
+﻿using GiveForGood.Web.Service.Models;
+
+namespace GiveForGood.Web.Service.Interfaces
+{
+    public interface IRoleService
+    {
+        Task<List<Role>> GetAllRoles();
+        Task<Role> GetRoleById(long id);
+        Task<bool> AddRole(Role role);
+        Task<bool> UpdateRole(long roleId,Role role);
+        Task<bool> DeleteRole(long id);
+    }
+}

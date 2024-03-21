d85d33cb9e92d5a88ba3478000fb1f64d169be09
sankar reddy avula <sankar.avula@betalen.in>
2024-03-21 15:52:18 +0530

    Db Configuration

---
+++ GiveForGood.Web.Service/DBConfiguration/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GiveForGood.Web.Service.DBConfiguration
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
